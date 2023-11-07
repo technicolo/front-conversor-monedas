c8b391c12cea9c906cd9c69f75d14a86dd9ce8df
technicolo <franfrare19@gmail.com>
2023-11-07 15:55:11 -0300

    creacion base del conversor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path:"login",
+  // canActivate: [usuarioSinLoguear],
+   loadChildren: ()=> import('./../app/public/pages/login/login.module').then(m => m.LoginModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './public/components/header/header.component';
+import { FooterComponent } from './public/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
